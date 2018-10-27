2e5cf4d90585ed099439eaa9f3e7b592c10828ac
Hugo Henry <hugo.henry95@free.fr>
2018-10-27 20:33:52 +0200

    test

--- main.go
+++ main.go
@@ -201,7 +201,7 @@ func main() {
 	idCount = 0
 	ids = nil
 	port := os.Getenv("PORT")
-	http.HandleFunc("/paragliding", getApi)
+	http.HandleFunc("/paragliding[/]{1}$", getApi)
 	http.HandleFunc("/paragliding/api", getApi)
 	http.HandleFunc("/paragliding/api/track/", igcHandler)
 	http.ListenAndServe(":"+port, nil)
